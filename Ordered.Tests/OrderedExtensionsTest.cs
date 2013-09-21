5e2c0a332509bbb318b0ca1b1d504579ef3c908d
vshashenko <v.shashenko@gmail.com>
2013-09-21 20:14:26 +0700

    Initial source import.

---
+++ Ordered.Tests/OrderedExtensionsTest.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ordered.Tests
+{
+    [TestClass]
+    public sealed class OrderedExtensionsTest
+    {
+        [TestMethod]
+        public void MergeGroupJoinOneToOne()
+        {
+            var outerCollection = new[] { 1, 3, 5 };
+            var innerCollection = new[] { 1, 3, 5 };
+
+            var expectedJoins = new[]
+            {
+                Tuple.Create(1, 1),
+                Tuple.Create(3, 3),
+                Tuple.Create(5, 5),
+            };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinOneToMany()
+        {
+            var outerCollection = new[] { 1, 3, 5 };
+            var innerCollection = new[] { 1, 1, 3, 5, 5, 5 };
+
+            var expectedJoins = new[]
+            {
+                Tuple.Create(1, 1), 
+                Tuple.Create(1, 1), 
+                Tuple.Create(3, 3), 
+                Tuple.Create(5, 5), 
+                Tuple.Create(5, 5), 
+                Tuple.Create(5, 5), 
+            };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinOneToManyWithChildless()
+        {
+            var outerCollection = new[] { 1, 3, 4, 5 };
+            var innerCollection = new[] { 1, 1, 3, 5, 5, 5 };
+
+            var expectedJoins = new[]
+                {
+                    Tuple.Create(1, 1), 
+                    Tuple.Create(1, 1), 
+                    Tuple.Create(3, 3), 
+                    Tuple.Create(5, 5), 
+                    Tuple.Create(5, 5), 
+                    Tuple.Create(5, 5), 
+                };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinOneToManyWithOrphans()
+        {
+            var outerCollection = new[] { 1, 3, 5 };
+            var innerCollection = new[] { 1, 1, 3, 4, 5, 5, 5 };
+
+            var expectedJoins = new[]
+                {
+                    Tuple.Create(1, 1), 
+                    Tuple.Create(1, 1), 
+                    Tuple.Create(3, 3), 
+                    Tuple.Create(5, 5), 
+                    Tuple.Create(5, 5), 
+                    Tuple.Create(5, 5), 
+                };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinOneToManyWithChildlessAndOrphans()
+        {
+            var outerCollection = new[] { 1, 3, 5 };
+            var innerCollection = new[] { 3, 3, 7, 7 };
+
+            var expectedJoins = new[]
+                {
+                    Tuple.Create(3, 3), 
+                    Tuple.Create(3, 3), 
+                };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinOuterEmpty()
+        {
+            var outerCollection = new int[] { };
+            var innerCollection = new[] { 3, 3, 5, 5 };
+
+            var expectedJoins = new Tuple<int, int>[] { };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinInnerEmpty()
+        {
+            var outerCollection = new[] { 1, 3, 3, 5 };
+            var innerCollection = new int[] { };
+
+            var expectedJoins = new Tuple<int, int>[] { };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinNoMatch()
+        {
+            var outerCollection = new[] { 1, 3, 5 };
+            var innerCollection = new[] { 2, 4, 7 };
+
+            var expectedJoins = new Tuple<int, int>[] { };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+
+        [TestMethod]
+        public void MergeGroupJoinEmptyCollections()
+        {
+            var outerCollection = new int[] { };
+            var innerCollection = new int[] { };
+
+            var expectedJoins = new Tuple<int, int>[] { };
+
+            var joins = new List<Tuple<int, int>>();
+
+            OrderedExtensions.GroupJoin(
+                outerCollection, innerCollection, i => i, j => j, (i, j) => joins.Add(Tuple.Create(i, j)));
+
+            CollectionAssert.AreEqual(expectedJoins, joins);
+        }
+    }
+}
---
+++ Ordered/OrderedExtensions.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ordered
+{
+    /// <summary>
+    /// Algorithms on ordered collections.
+    /// Thus all input collections must be sorted to get correct result.
+    /// </summary>
+    public static class OrderedExtensions
+    {
+        /// <summary>
+        /// Subtracts second collection from first collection.
+        /// The complexity is O(n).
+        /// </summary>
+        public static IEnumerable<T> Difference<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2)
+        {
+            return Difference(seq1, seq2, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Subtracts second collection from first collection.
+        /// The complexity is O(n).
+        /// </summary>
+        public static IEnumerable<T> Difference<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2, IComparer<T> comparer)
+        {
+            var enum1 = seq1.GetEnumerator();
+            var enum2 = seq2.GetEnumerator();
+
+            var b1 = enum1.MoveNext();
+            var b2 = enum2.MoveNext();
+
+            while (b1 && b2)
+            {
+                if (comparer.Compare(enum1.Current, enum2.Current) < 0)
+                {
+                    yield return enum1.Current;
+                    b1 = enum1.MoveNext();
+                }
+                else if (comparer.Compare(enum2.Current, enum1.Current) < 0)
+                {
+                    b2 = enum2.MoveNext();
+                }
+                else
+                {
+                    b1 = enum1.MoveNext();
+                    b2 = enum2.MoveNext();
+                }
+            }
+
+            // return the rest of the first collection
+            while (b1)
+            {
+                yield return enum1.Current;
+                b1 = enum1.MoveNext();
+            }
+        }
+
+        /// <summary>
+        /// Intersects two collections. Equal elements output from the first collection.
+        /// The complexity is O(n).
+        /// </summary>
+        public static IEnumerable<T> Intersect<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2)
+        {
+            return Intersect(seq1, seq2, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Intersects two collections. Equal elements output from the first collection.
+        /// The complexity is O(n).
+        /// </summary>
+        public static IEnumerable<T> Intersect<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2, IComparer<T> comparer)
+        {
+            var enum1 = seq1.GetEnumerator();
+            var enum2 = seq2.GetEnumerator();
+
+            var b1 = enum1.MoveNext();
+            var b2 = enum2.MoveNext();
+
+            while (b1 && b2)
+            {
+                if (comparer.Compare(enum1.Current, enum2.Current) < 0)
+                {
+                    b1 = enum1.MoveNext();
+                }
+                else if (comparer.Compare(enum2.Current, enum1.Current) < 0)
+                {
+                    b2 = enum2.MoveNext();
+                }
+                else
+                {
+                    yield return enum1.Current;
+
+                    b1 = enum1.MoveNext();
+                    b2 = enum2.MoveNext();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Unions two collections. Equal elements output from the first collection.
+        /// The complexity is O(n).
+        /// </summary>
+        public static IEnumerable<T> Union<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2)
+        {
+            return Union(seq1, seq2, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Unions two collections. Equal elements output from the first collection.
+        /// The complexity is O(n).
+        /// </summary>
+        public static IEnumerable<T> Union<T>(this IEnumerable<T> seq1, IEnumerable<T> seq2, IComparer<T> comparer)
+        {
+            var enum1 = seq1.GetEnumerator();
+            var enum2 = seq2.GetEnumerator();
+
+            var b1 = enum1.MoveNext();
+            var b2 = enum2.MoveNext();
+
+            while (b1 && b2)
+            {
+                if (comparer.Compare(enum1.Current, enum2.Current) < 0)
+                {
+                    yield return enum1.Current;
+                    b1 = enum1.MoveNext();
+                }
+                else if (comparer.Compare(enum2.Current, enum1.Current) < 0)
+                {
+                    yield return enum2.Current;
+                    b2 = enum2.MoveNext();
+                }
+                else
+                {
+                    yield return enum1.Current;
+
+                    b1 = enum1.MoveNext();
+                    b2 = enum2.MoveNext();
+                }
+            }
+        }
+
+        public static void GroupJoin<TOuter, TInner, TKey>(
+            this IEnumerable<TOuter> outer,
+            IEnumerable<TInner> inner,
+            Func<TOuter, TKey> outerKeySelector,
+            Func<TInner, TKey> innerKeySelector,
+            Action<TOuter, TInner> joinAction)
+        {
+            GroupJoin(outer, inner, outerKeySelector, innerKeySelector, joinAction, Comparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// Joins two collections as child-parent relationship by the specified key.
+        /// Collections must be sorted by the key on which the join is performed.
+        /// The complexity is O(n).
+        /// </summary>
+        /// <param name="outer">Outer collection. Contains parent elements.</param>
+        /// <param name="inner">Inner collection. Contains child elements.</param>
+        /// <param name="outerKeySelector">Key selector for the outer collection.</param>
+        /// <param name="innerKeySelector">Key selection for the inner collection.</param>
+        /// <param name="joinAction">Action performed on key equality between outer and inner collections.</param>
+        /// <param name="comparer"></param>
+        public static void GroupJoin<TOuter, TInner, TKey>(
+            this IEnumerable<TOuter> outer,
+            IEnumerable<TInner> inner,
+            Func<TOuter, TKey> outerKeySelector,
+            Func<TInner, TKey> innerKeySelector,
+            Action<TOuter, TInner> joinAction,
+            IComparer<TKey> comparer)
+        {
+            var enum1 = outer.GetEnumerator();
+            var enum2 = inner.GetEnumerator();
+
+            var b1 = enum1.MoveNext();
+            var b2 = enum2.MoveNext();
+
+            while (b1 && b2)
+            {
+                var item1 = enum1.Current;
+                var item2 = enum2.Current;
+
+                var order = comparer.Compare(outerKeySelector(item1), innerKeySelector(item2));
+
+                if (order < 0)
+                {
+                    b1 = enum1.MoveNext();
+                }
+                else if (order > 0)
+                {
+                    b2 = enum2.MoveNext();
+                }
+                else
+                {
+                    joinAction(item1, item2);
+                    b2 = enum2.MoveNext();
+                }
+            }
+        }
+    }
+}
